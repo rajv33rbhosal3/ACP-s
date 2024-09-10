bc748adc82e8a9a62845efb071e3d9e6a053f91c
Rajveer Bhosale <rajveerbhosale92@gmail.com>
2024-09-10 22:31:40 +0530

    ACP

---
+++ ACP.py
@@ -0,0 +1,13 @@
+def P8(n):
+    if n == 0:
+        return False
+    else:
+        
+        while n%8 == 0:
+            n = n/8
+    return n==1
+n = int(input("Enter a number : "))
+if P8(n):
+    print("Number is power of 8")
+else:
+    print("Numebr is not the power of 8")
